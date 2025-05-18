ea81524be0db06dd8cd5dac7d3137c46446edc70
zeevarblocks <iiruna093@gmail.com>
2025-05-19 07:16:07 +0800

    Initial commit

---
+++ components/FloatingButton.js
@@ -0,0 +1,6 @@
+<Link
+  href="/rewards"
+  className="fixed bottom-5 right-5 bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-3 px-5 rounded-full shadow-lg z-50"
+>
+  Claim Rewards
+</Link>
