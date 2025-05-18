665375d8aed8c33258a90129810d164d39dd407b
zeevarblocks <iiruna093@gmail.com>
2025-05-18 22:40:00 +0800

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
-// pages/_app.js
 import '../styles/globals.css';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <nav style={{
+        padding: '1rem',
+        backgroundColor: '#000',
+        color: '#fff',
+        display: 'flex',
+        gap: '1rem'
+      }}>
+        <Link href="/" style={{ color: '#fff', textDecoration: 'none' }}>
+          Home
+        </Link>
+        <Link href="/rewards" style={{ color: '#fff', textDecoration: 'none' }}>
+          Rewards
+        </Link>
+      </nav>
       <Component {...pageProps} />
       <Analytics />
       <SpeedInsights />
