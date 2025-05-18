463081c85bcad74406985c23a160eb6b5524bced
zeevarblocks <iiruna093@gmail.com>
2025-05-19 07:23:32 +0800

    Initial commit

--- components/FloatingButton.js
+++ components/FloatingButton.js
@@ -1,6 +1,13 @@
-<Link
-  href="/rewards"
-  className="fixed bottom-5 right-5 bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-3 px-5 rounded-full shadow-lg z-50"
->
-  Claim Rewards
-</Link>
+// components/FloatingButton.js
+import Link from 'next/link';
+
+export default function FloatingButton() {
+  return (
+    <Link
+      href="/rewards"
+      className="fixed bottom-6 right-6 bg-yellow-400 text-black font-bold px-6 py-3 rounded-full shadow-lg hover:bg-yellow-500 transition z-50"
+    >
+      Claim Rewards
+    </Link>
+  );
+}
