d32ff3e4719ad0e62515688afd8df62ccf5b4f45
zeevarblocks <iiruna093@gmail.com>
2025-05-19 23:45:58 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import FloatingButton from '../components/FloatingButton';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import BTCChart from '../components/BTCChart';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -10,6 +11,8 @@ function MyApp({ Component, pageProps }) {
       <Navbar />
       <main className="p-6 min-h-screen">
         <Component {...pageProps} />
+    h1>Bitcoin Analyzer</h1> 
+      <BTCChart />
       </main>
       <FloatingButton />
       <Analytics />
