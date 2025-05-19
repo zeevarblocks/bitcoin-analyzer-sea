df2c081bf62d5edd7468573f937e1fd2e00e37a0
zeevarblocks <iiruna093@gmail.com>
2025-05-19 23:52:42 +0800

    Create chart.js

---
+++ pages/chart.js
@@ -0,0 +1,10 @@
+import BTCChart from '../components/BTCChart';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Bitcoin Analyzer</h1>
+      <BTCChart />
+    </main>
+  );
+}
