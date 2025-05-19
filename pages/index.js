8786a28531b9733a49f94f232c1f14d56a49718e
zeevarblocks <iiruna093@gmail.com>
2025-05-19 23:35:23 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,7 @@ Chart.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Too
 import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
 import { fetchBTCData } from '../utils/fetchBTCData';
-import dynamic from 'next/dynamic';
-
-const BTCChart = dynamic(() => import('../components/BTCChart'), { ssr: false });
+import BTCChart from '../components/BTCChart';
 
 export default function Home() {
   const [ath, setAth] = useState('');
