d8fa427fe85ed244a1656fb36cdce5db28c4a12d
zeevarblocks <iiruna093@gmail.com>
2025-05-20 11:03:40 +0000

    initial commit

--- pages/signal-analyzer.js
+++ pages/signal-analyzer.js
@@ -14,7 +14,7 @@ import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
 import { fetchBTCData } from '../utils/fetchBTCData';
 
-export default function Home() {
+export default function About() {
   const [ath, setAth] = useState('');
   const [atl, setAtl] = useState('');
   const [ema70, setEma70] = useState('');
