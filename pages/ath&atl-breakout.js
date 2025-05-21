79e8a635aaa1ac55f9729ce4954072a0dcbc547a
zeevarblocks <iiruna093@gmail.com>
2025-05-21 05:40:31 +0000

    initial commit

--- pages/ath&atl-breakout.js
+++ pages/ath&atl-breakout.js
@@ -1,27 +1,43 @@
 import {
   computeAthBreakoutSignal,
-  computeAtlBreakoutSignal // Updated import
+  computeAtlBreakoutSignal
 } from '../utils/ath&atlBreakout';
+import { useEffect, useState } from 'react';
+import { fetchMarketData } from '../utils/fetchMarketData';
 
 export default function BreakoutPage() {
+  const [marketData, setMarketData] = useState({ currentPrice: null, ema70: null });
+
+  useEffect(() => {
+    async function getData() {
+      const data = await fetchMarketData();
+      setMarketData(data);
+    }
+    getData();
+  }, []);
+
   const athResult = computeAthBreakoutSignal({
-    currentPrice: 73000,
+    currentPrice: marketData.currentPrice || 73000,
     previousAth: 69000,
-    ema70: 71000,
+    ema70: marketData.ema70 || 71000,
     athBreakoutDate: '2024-03-11',
     previousAthDate: '2021-11-08'
   });
 
-  const atlResult = computeAtlBreakoutSignal({ // Updated function call
-    currentPrice: 15000,
+  const atlResult = computeAtlBreakoutSignal({
+    currentPrice: marketData.currentPrice || 15000,
     previousAtl: 17000,
-    ema70: 19000,
-    atlBreakoutDate: '2023-12-01', // Renamed to match updated function
+    ema70: marketData.ema70 || 19000,
+    atlBreakoutDate: '2023-12-01',
     previousAtlDate: '2022-11-08'
   });
 
   return (
     <div>
+      <h1>Bitcoin Market Data</h1>
+      <p>Current Price: ${marketData.currentPrice}</p>
+      <p>EMA70: ${marketData.ema70}</p>
+
       <h1>ATH Breakout Signal</h1>
       <p>Signal: {athResult.signal}</p>
       <p>Weeks Since Previous ATH: {athResult.weeksSincePreviousAth}</p>
