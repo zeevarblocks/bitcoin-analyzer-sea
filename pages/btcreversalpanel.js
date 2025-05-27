dee1734bdb881bb26af41ef4944e587f1c8f30c8
zeevarblocks <iiruna093@gmail.com>
2025-05-27 14:10:00 +0800

    Update btcreversalpanel.js

--- pages/btcreversalpanel.js
+++ pages/btcreversalpanel.js
@@ -91,9 +91,9 @@ export default function BitcoinSignalAnalyzer() {
           color: 'white',
           maxWidth: '600px',
           width: '100%',
-          textAlign: 'center',
-        	text-white
+          textAlign: 'center',	
         }}
+className="text-white"
       >
             <h1 className="text-3xl font-bold text-center text-gray-900"> ATH/ATL vs EMA70 </h1>
 
