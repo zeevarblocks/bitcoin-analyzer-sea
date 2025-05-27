167c356c5498e2d942b6915162fe8ca2e30dd161
zeevarblocks <iiruna093@gmail.com>
2025-05-27 12:11:37 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,9 +10,9 @@ function MyApp({ Component, pageProps }) {
     <>
       <Navbar />
       <main className="min-h-screen p-6 flex justify-center items-start">
-        <div className="container bg-black/60 backdrop-blur-md text-gray-800 rounded-2xl shadow-lg w-full max-w-4xl">
-          <Component {...pageProps} />
-        </div>
+        <div className="container w-full max-w-4xl rounded-2xl shadow-2xl bg-gradient-to-br from-purple-600 via-pink-500 to-red-500 text-white p-6">
+  <Component {...pageProps} />
+</div>
       </main>
       <FloatingButton />
       <Analytics />
