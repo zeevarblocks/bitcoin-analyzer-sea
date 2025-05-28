b623c5b7f78fe983ad6585607a5ac23e675235f2
zeevarblocks <iiruna093@gmail.com>
2025-05-28 14:59:22 +0800

    Update bitcoin-signal-analyzer.js

--- pages/bitcoin-signal-analyzer.js
+++ pages/bitcoin-signal-analyzer.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { AlertTriangle, AlertCircle } from "lucide-react";
 import { Alert, AlertDescription, AlertTitle } from "../components/Alert";
+import axios from 'axios';
 
 
 
