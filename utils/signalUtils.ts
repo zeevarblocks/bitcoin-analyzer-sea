7a25c3ab8a8d13b4a8e446ec278c397c8177a1af
zeevarblocks <iiruna093@gmail.com>
2025-05-29 07:54:00 +0800

    Delete utils/signalUtils.ts

--- utils/signalUtils.ts
+++
@@ -1,7 +0,0 @@
-export function findRelevantLevel(prices: number[], type: 'support' | 'resistance'): number {
-  if (type === 'support') {
-    return Math.min(...prices);
-  } else {
-    return Math.max(...prices);
-  }
-}
