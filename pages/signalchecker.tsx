0911b03e66f6cf600454b002f681bf4261be942c
zeevarblocks <iiruna093@gmail.com>
2025-05-31 12:04:13 +0800

    Update signalchecker.tsx

--- pages/signalchecker.tsx
+++ pages/signalchecker.tsx
@@ -24,6 +24,7 @@ bullishContinuation: boolean;
   intradayLowerLowBreak: boolean;
   todaysLowestLow: number; 
   todaysHighestHigh: number;  
+url: string;
 }
 
 // fetchCandles, calculateEMA, etc.,.
