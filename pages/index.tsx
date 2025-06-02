f2c781f1e960e7de456c86fbb8eb8b6c75d37e25
zeevarblocks <iiruna093@gmail.com>
2025-06-02 20:55:05 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -489,8 +489,6 @@ const detectionOption = {
   minVolume: 1000,
 };
 
-const result = detectBearishContinuation(closes, highs, lows, ema70, ema14, rsi14, ema200, volumes, detectionOptions); 
-    
 
 if (trend === 'bearish') {
   const result = detectBearishContinuation(
