2e90bee9f497c05289453f8b7eee9b9dbc0f3953
zeevarblocks <iiruna093@gmail.com>
2025-06-07 12:47:01 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
-      <main className="min-h-screen p-10 flex justify-center items-start">
+      <main className="min-h-screen p-6 flex justify-center items-start">
         
           <Component {...pageProps} />
         
